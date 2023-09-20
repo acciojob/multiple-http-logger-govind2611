0a8c0eb96baecfdb18392e8e816748e58cfaba51
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-09-20 08:00:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,5 +4,27 @@ const urls = [
   "https://jsonplaceholder.typicode.com/posts/3",
 ];
 
-// Don't change the above line
-// Write your code here
+// Function to make an HTTP request to a URL and log the response
+async function fetchAndLog(url) {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(`Response from ${url}:`, data);
+    } else {
+      console.error(`Error fetching ${url}: ${response.status} - ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error(`Error fetching ${url}: ${error.message}`);
+  }
+}
+
+// Loop through the URLs and make asynchronous requests
+async function fetchUrls() {
+  for (const url of urls) {
+    await fetchAndLog(url);
+  }
+}
+
+// Start fetching the URLs
+fetchUrls();
